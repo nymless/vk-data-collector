c2b4a2c973b5d7567722e35efe462c0e4eeed411
Ivan Baklanov <nymless.ib@gmail.com>
2025-03-25 12:47:55 +0500

    Fix circular imports issue

--- src/lib/types/methods/wall_get_comments.py
+++ src/lib/types/methods/wall_get_comments.py
@@ -1,13 +1,13 @@
 from typing import TypedDict
 
-from lib.types.objects.comment import Comment
+from lib.types.objects.comment import Comment, TreadComment
 from lib.types.objects.group import Group
 from lib.types.objects.user import User
 
 
 class Response(TypedDict):
     count: int
-    items: list[Comment]
+    items: list[Comment | TreadComment]
     current_level_count: int
     can_post: bool
     show_reply_button: bool
--- src/lib/types/objects/comment.py
+++ src/lib/types/objects/comment.py
@@ -1,7 +1,27 @@
-from typing import Any, TypedDict
+from typing import Any, Optional, TypedDict
 
 from lib.types.objects.donut import Donut
-from lib.types.objects.thread import Thread
+
+
+class TreadComment(TypedDict):
+    id: int
+    from_id: int
+    date: int
+    text: str
+    post_id: int
+    owner_id: int
+    parents_stack: list[int]
+    reply_to_user: Optional[int]
+    reply_to_comment: Optional[int]
+
+
+class Thread(TypedDict):
+    count: int
+    items: list[TreadComment]
+    can_post: bool
+    show_reply_button: bool
+    groups_can_post: bool
+    next_from: str
 
 
 class Comment(TypedDict):
@@ -9,9 +29,9 @@ class Comment(TypedDict):
     from_id: int
     date: int
     text: str
-    donut: Donut
-    reply_to_user: int
-    reply_to_comment: int
-    attachments: list[dict[str, Any]]
-    parents_stack: list[Any]
+    donut: Optional[Donut]
+    attachments: Optional[list[dict[str, Any]]]
+    parents_stack: list[int]
     thread: Thread
+    reply_to_user: Optional[int]
+    reply_to_comment: Optional[int]
